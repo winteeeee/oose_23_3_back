8a3f38ca9dadc23d474fde1e8db40f788e77117a
wintee <b12e34e56@naver.com>
2023-05-26 19:52:44 +0900

    백엔드 개발 시작

---
+++ src/main/java/com/example/oose_23_3_back/Oose233BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.oose_23_3_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oose233BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oose233BackApplication.class, args);
+	}
+
+}
