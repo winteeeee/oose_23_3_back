bb2f1aa7ab561525ebe2f940ed51b075e6bebb3d
wintee <b12e34e56@naver.com>
2023-06-02 01:20:02 +0900

    [운영관리 :: AI 속성 삭제, RequestBody 어노테이션 추가]

--- src/main/java/com/example/oose_23_3_back/managementcontrol/control/BicycleControl.java
+++ src/main/java/com/example/oose_23_3_back/managementcontrol/control/BicycleControl.java
@@ -4,10 +4,7 @@ import com.example.oose_23_3_back.managementcontrol.entity.RentalOffice;
 import com.example.oose_23_3_back.managementcontrol.service.BicycleService;
 import com.example.oose_23_3_back.managementcontrol.entity.Bicycle;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,7 +14,8 @@ public class BicycleControl {
     private final BicycleService bicycleService;
 
     @PostMapping("/bicycleInsert")
-    public void bicycleSave(Bicycle bicycle) {
+    public void bicycleSave(@RequestBody Bicycle bicycle) {
+        System.out.println(bicycle.toString());
         this.bicycleService.bicycleSave(bicycle);
     }
 
