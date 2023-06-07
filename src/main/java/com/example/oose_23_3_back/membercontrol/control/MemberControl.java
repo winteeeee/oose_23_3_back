68a122ab8612b02527449e5ad8a8c1e1cd6d4ae5
winteeeee <b12e34e56@naver.com>
2023-06-07 19:58:01 +0900

    [회원관리 :: 로그인 구현, 테스트 필요]

--- src/main/java/com/example/oose_23_3_back/membercontrol/control/MemberControl.java
+++ src/main/java/com/example/oose_23_3_back/membercontrol/control/MemberControl.java
@@ -2,10 +2,10 @@ package com.example.oose_23_3_back.membercontrol.control;
 
 import com.example.oose_23_3_back.membercontrol.service.MemberService;
 import com.example.oose_23_3_back.membercontrol.entity.Member;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +22,18 @@ public class MemberControl {
     @GetMapping("/memberFindAll")
     public List<Member> memberSelect() {
         return this.memberService.getMember();
+    }
+
+    @PostMapping("/memberLogin")
+    public boolean memberLogin(@RequestBody Member m, HttpServletRequest req) {
+        Member loginTarget = this.memberService.getMember(m);
+
+        if (loginTarget != null) {
+            HttpSession session = req.getSession();
+            session.setAttribute("sessionId", m);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/main/java/com/example/oose_23_3_back/membercontrol/repository/MemberRepository.java
+++ src/main/java/com/example/oose_23_3_back/membercontrol/repository/MemberRepository.java
@@ -4,5 +4,5 @@ import com.example.oose_23_3_back.membercontrol.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
-    Member findByIdentification(String id);
+    Member findByIdAndPassword(String id, String password);
 }
